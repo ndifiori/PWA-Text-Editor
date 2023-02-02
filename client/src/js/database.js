5a29d25219a90b7d0cb075ee17f7ee0c4eb5ea5e
Nathan Difiori <ndifiori@gmail.com>
2023-02-01 18:16:00 -0800

    edited service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.createObjectStore('jate');
+  const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('data saved to the databse', result.value)
@@ -28,6 +28,7 @@ export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readOnly');
+  const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result ? console.log('data retrived from the database', result.value) : console.log('data not found in database')
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file above
+// TODO: Add CSS loaders and babel to webpack below
 
 module.exports = () => {
   return {
@@ -24,7 +24,7 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: '.src-sw.js'
+        swDest: 'src-sw.js'
       }),
       new WebpackPwaManifest({
         fingerprints: false,
